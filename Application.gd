e65a836fecda2b0f8814ee767108363ccdd6eb89
Luke Miller <luke@lukeamiller.net>
2022-05-19 12:44:44 -0700

    CDX-5 CDX-6 CDX-7 - Changes completed for all three issues, comiting to enable their closure

--- Application.gd
+++ Application.gd
@@ -1,9 +1,17 @@
 extends PanelContainer
 
-
+# Declare constants
 const NOTEBOOKS_BASE_PATH = "user://notebooks/"
 
+# Hold references to relevant nodes for easier use
+var document_pane
+var text_editor_pane
+
 func _ready():
+	# Populate the node reference variables
+	document_pane = $ApplicationContainer/MainContainer/ApplicationPanes/DocumentPane
+	text_editor_pane = $ApplicationContainer/MainContainer/ApplicationPanes/TextEditorPane
+	
 	# Check the user directory for the notebooks folder, create if missing
 	var dir = Directory.new()
 	
@@ -28,8 +36,14 @@ func _on_NewNotebookButton_pressed():
 # Create a new Notebook and refresh the document tree
 func _on_new_notebook(name):
 	new_notebook(name)
-	
-	
+
+
+# Open an activated document
+func _on_DocumentPane_document_activated(document):
+	# TODO: Hand this behavior off to the TextEditorContainer for handling
+	text_editor_pane.load_document(document)
+
+
 func new_notebook(name):
 	print("Creating a new notebook with the name " + name)
 	var dir = Directory.new()
@@ -41,10 +55,12 @@ func new_notebook(name):
 		
 		# Switch the document tree to this notebook
 		print("Switching document tree to new notebook")
-		$ApplicationContainer/MainContainer/ApplicationPanes/DocumentPane.switch_notebook(name)
+		document_pane.refresh_pane()
+		document_pane.switch_notebook(name)
 	else:
 		print("This notebook exists already, fire an error toast and change to display it")
-		$ApplicationContainer/MainContainer/ApplicationPanes/DocumentPane.switch_notebook(name)
+		document_pane.refresh_pane()
+		document_pane.switch_notebook(name)
 
 
 # TEXT EDITOR PANE FUNCTIONS
--- TextEditorPane.gd
+++ TextEditorPane.gd
@@ -1,3 +1,21 @@
 extends PanelContainer
 
 
+# Hold references to relevant nodes
+var current_filename
+var text_editor
+
+
+func _ready():
+	# Populate the references to relevant nodes
+	current_filename = $TextEditorContainer/FilenameContainer/FilenameLabel
+	text_editor = $TextEditorContainer/TextEditorPanel/TextEditor
+
+
+func load_document(document):
+	# Clear the contents of the text editor
+	text_editor.text = ""
+	text_editor.text = document
+	
+	# TODO: Parse the in-document headers for the incoming document
+	
